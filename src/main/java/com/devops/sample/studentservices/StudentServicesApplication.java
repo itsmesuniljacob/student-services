6be6ed7f4ab6411f44f085e745c64dde9e6e1fca
Sunil.Jacob <sunil1405@gmail.com>
2018-08-16 12:09:08 +0530

    Java Project

---
+++ src/main/java/com/devops/sample/studentservices/StudentServicesApplication.java
@@ -0,0 +1,12 @@
+package com.devops.sample.studentservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesApplication.class, args);
+	}
+}
